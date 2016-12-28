da1fce01110720ba950646e5851efd499ec9d586
zebesta <chris.sebesta@gmail.com>
2016-12-28 15:40:22 -0600

    refactors function calls and added image URL recognition

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors')
+var bodyParser = require('body-parser');
 
 var imagerec = require('./imagerec');
 var scraper = require('./scraper');
@@ -7,6 +8,10 @@ var scraper = require('./scraper');
 var app = express();
 app.use(express.static('public'))
 app.use(cors());
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 
 // image based query to pull dominate image recognition then tags
@@ -16,6 +21,20 @@ app.get('/', function(req, res) {
 
 app.get('/img', function(req, res) {
   var hello = imagerec('http://cdn2-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-8.jpg');
+  console.log(hello);
+  hello.then(results=>{
+    console.log("RESOLVING ON SERVER");
+    console.log(hello);
+    res.json(results);
+  })
+  .catch(err=>{
+    res.json("Holy shit, theres been an error!");
+  });
+});
+app.post('/img/url', function(req, res) {
+  var imgUrl = req.body.imgUrl;
+  console.log(imgUrl);
+  var hello = imagerec(imgUrl); //imagerec('http://cdn2-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-8.jpg');
   console.log(hello);
   hello.then(results=>{
     console.log("RESOLVING ON SERVER");
